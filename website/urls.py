d3642f3c2ef6c0060a482f0f9715141368239792
Moooogz <markdavedizon@yahoo.com>
2023-08-26 15:42:15 +0800

    Refine MAinpage

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
-    path('', views.login, name='login'),
+    path('', views.login_user, name='login'),
     path('home/', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
+    path('mainpage/', views.mainPage, name='mainpage'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html',{})
+    if request.user.is_authenticated:
+        return render(request, 'home.html',{})   
+    else:
+        return render(request, 'login.html',{})
+    
 
-def login(request):
-    return render(request, 'login.html',{})
+def login_user(request):
+    if request.method == 'POST':
+        uNmae = request.POST['uName']
+        pWord = request.POST['pWord']
+
+        user = authenticate(request, username=uNmae, password=pWord)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You have been login!")
+            return redirect('mainpage')            
+        else:
+            messages.success(request,"Wrong Credentials!")
+            return render(request, 'login.html',{})
+    else:
+        return render(request, 'login.html',{})
+
+   
+    
+def mainPage(request):
+    return render(request, 'mainpage.html',{})
+
+
+def logout_user(request):
+    logout(request)
+    return render(request, 'logout.html',{})
+   
